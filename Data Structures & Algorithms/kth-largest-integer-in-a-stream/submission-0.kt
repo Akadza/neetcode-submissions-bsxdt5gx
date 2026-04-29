225e8c80ca9e74618471296ed42dc2375df284fd
Александр <61403845+Akadza@users.noreply.github.com>
2026-04-29 08:06:04 +0000

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.kt
@@ -0,0 +1,12 @@
+class KthLargest(val k: Int, nums: IntArray) {
+    private val minHeap = PriorityQueue<Int>()
+    init {
+        for (num in nums) add(num)
+    }
+
+    fun add(`val`: Int): Int {
+        minHeap.offer(`val`)
+        if (minHeap.size > k) minHeap.poll()
+        return minHeap.peek()
+    }
+}
