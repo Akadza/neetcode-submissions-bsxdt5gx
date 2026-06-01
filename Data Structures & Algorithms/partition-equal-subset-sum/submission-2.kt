4afa41e54e8b676532597b839a564cf4653e791a
Александр <61403845+Akadza@users.noreply.github.com>
2026-06-01 08:59:10 +0000

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun canPartition(nums: IntArray): Boolean {
+        val totalSum = nums.sum()
+        if (totalSum % 2 == 1) return false
+        val target = totalSum / 2
+
+        val memo = BooleanArray(target + 1)
+        memo[0] = true
+
+        for (num in nums) {
+            for (i in target downTo num) {
+                if (memo[i - num]) memo[i] = true
+            }
+            if (memo[target]) return true
+        }
+        return memo[target]
+    }
+}
